91dec701c2cd4c4b04e6038e06a7040969623461
pyrotheumgit <pyrotheum1702@gmail.com>
2023-03-17 09:51:04 +0700

    lesson 1: conditional statement

---
+++ 4-conditional-statement/README.md
@@ -0,0 +1,12 @@
+# Content Overview
+  1. Conditional Statements
+  2. Switch Case Statement
+# Conditional Statements
+  1. Lesson content:
+  - If statement
+  - Else statement
+  - Elif statement
+  2. What i learned:
+  - How to implement an if statement
+  - How to implement an else statement
+  - How to implement an elif statement
---
+++ 4-conditional-statement/conditional-statement.py
@@ -0,0 +1,19 @@
+# If statement implementation
+if 2 < 5:
+    print(True)
+
+# Result: 2 < 5 is True
+
+# Else statement implementation
+if 2 > 5:
+    print(True)
+else:
+    print(False)
+# Result: 2 < 5 is False
+
+# Elif statement implementation
+if 2 > 5:
+    print(True)
+elif 2 > 0:
+    print(True)
+# Result: 2 > 5 is False but 2 > 0 is True
