f7151d99ea0184c5480a8ad8d24f1abc147bb985
pyrotheumgit <pyrotheum1702@gmail.com>
2023-03-17 08:33:23 +0700

    lesson 1: python variable

---
+++ 3-variable-and-data-types/python_variable.py
@@ -0,0 +1,25 @@
+# What is a variable
+var = "variable"
+# var is the variable
+# Result = "variable"
+
+# Assign variable
+var_1 = "variable"
+print(var_1)
+# Result var_1 = "variable"
+
+# Re-assign variable
+var_1 = "variable_1"
+print(var_1)
+# Result var_1 = "variable_1"
+
+# Re-assign variable with different type of data
+var_1 = 1
+print(var_1)
+# Result var_1 = 1
+
+# Chain assign variable
+var2, var_3 = 3.14
+print(var2, var_3)
+# Result var2 = 3.14
+# Result var_3 = 3.14
