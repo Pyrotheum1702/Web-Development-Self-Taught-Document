9f636df292972294c4f81c6e62ac7027d7ef86e3
pyrotheumgit <pyrotheum1702@gmail.com>
2023-03-17 09:53:29 +0700

    lesson 2: switch case statement

---
+++ 4-conditional-statement/switch_case_statement.py
@@ -0,0 +1,15 @@
+animal_name = "Tiger"
+
+# Switch statement implementation
+match animal_name:
+    case "Lion":
+        print("Lion case")
+    case "Elephant":
+        print("Elephant case")
+    case "Leopard":
+        print("Leopard case")
+    case "Tiger":
+        print("Tiger case")
+    case "Cow":
+        print("Cow case")
+# Result: case "Tiger"
